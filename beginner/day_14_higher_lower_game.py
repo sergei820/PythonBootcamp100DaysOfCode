bfd9aadd96d4c8e103b0fa3e5f8c1f6b353f1d18
serje <serje@mayflower.work>
2024-08-19 19:21:39 +0300

    Day 14 - implementation plan done

--- beginner/day_14_higher_lower_game.py
+++ beginner/day_14_higher_lower_game.py
@@ -2,8 +2,20 @@ import utilities.game_data
 
 
 def play_higher_lower():
+    # TODO-1 collect the data from the dict
+
+    # TODO-2 parse dict, randomly getting comparing persons
+
+    # TODO-3 print person 1 data & person 2 data
+
+    # TODO-4 ask user who has more followers
     game = utilities.game_data
 
+    # TODO-5 check the answer
+
+    # TODO-6 if user won, ask the next question, score++
+
+    # TODO-7 if the user lost, ask if they want to try again
 
 if __name__ == '__main__':
     play_higher_lower()

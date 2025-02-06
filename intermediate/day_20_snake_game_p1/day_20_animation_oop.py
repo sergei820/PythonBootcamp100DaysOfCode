a724572faebd6fae276696f85548674adeb79e1c
Sergei <se25051990@gmail.com>
2025-02-06 08:18:51 +0200

    Day 20 - in progress

--- intermediate/day_20_snake_game_p1/day_20_animation_oop.py
+++ intermediate/day_20_snake_game_p1/day_20_animation_oop.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,17 +8,7 @@ screen.bgcolor("black")
 screen.title("Snake game")
 screen.tracer(0)
 
-coordinates = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in coordinates:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_is_on = True
 
@@ -25,11 +16,6 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments)-1, 0, -1):
-        new_x = segments[seg_num-1].xcor()
-        new_y = segments[seg_num-1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(10)
-
+    snake.move()
 
 screen.exitonclick()

5b3ac687496ef95c6fd0671c88fa9d2cfcbc51e8
Sergei <se25051990@gmail.com>
2025-02-11 22:21:13 +0200

    Day 21 - Done

--- intermediate/day_21_snake_game_p2/food.py
+++ intermediate/day_21_snake_game_p2/food.py
@@ -7,7 +7,7 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.shapesize(stretch_len=0.8, stretch_wid=0.8)
         self.color("blue")
         self.speed("fastest")
         rand_x = random.randint(-280, 280)
--- intermediate/day_21_snake_game_p2/snake.py
+++ intermediate/day_21_snake_game_p2/snake.py
@@ -12,19 +12,25 @@ class Snake:
         self.segments = []
 
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
-
+            self.add_segment(position)
         self.head = self.segments[0]
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend_snake(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
+
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):

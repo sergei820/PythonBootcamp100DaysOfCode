29576541db768d951eb76f72e5c22ff84961b0f9
serje <serje@mayflower.work>
2025-04-23 19:55:59 +0300

    Day 25 - started

--- intermediate/day_25_csv_pandas/main.py
+++ intermediate/day_25_csv_pandas/main.py
@@ -1,2 +1,9 @@
 def parse_csv(filename: str):
-    pass
+    lines_list = []
+    with open(filename, 'r') as file:
+        lines_list = file.readlines()
+
+    return lines_list
+
+if __name__ == "__main__":
+    print(parse_csv("./weather_data.csv"))

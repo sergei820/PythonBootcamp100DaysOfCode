24f58df228942c7f9350112628d4365b31b962c2
Sergei <se25051990@gmail.com>
2025-04-25 21:20:50 +0300

    Day 25 - in progress

--- intermediate/day_25_csv_pandas/day-25-us-states-game-start/main.py
+++ intermediate/day_25_csv_pandas/day-25-us-states-game-start/main.py
@@ -1,14 +1,44 @@
 import turtle
+import pandas
 
-screen = turtle.Screen()
-screen.title("U.S. States Game")
+def start_app():
+    file_us_states = '50_states.csv'
+    screen = turtle.Screen()
+    screen.title("U.S. States Game")
 
-image="blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
+    image="blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+    t_text = turtle.Turtle()
+    t_text.hideturtle()
+    t_text.penup()
+
+    answered_states = []
+    data = pandas.read_csv(file_us_states)
+    states_list = list(data.state)
+
+    while len(answered_states) < 50:
+
+        answer_state = (screen.textinput(
+            title=f"{len(answered_states)}/50 States Correct",
+            prompt="What's another state's name?"
+        ))
+
+        if answer_state is None:
+            break
+        else:
+            answer_state = answer_state.title()
+
+        if answer_state in states_list:
+            state_row = data[data.state == answer_state]
+            t_text.goto(float(state_row.x.iloc[0]), float(state_row.y.iloc[0]))
+            t_text.write(answer_state, align="center", font=("Arial", 8, "normal"))
+            if answer_state not in answered_states:
+                answered_states.append(answer_state)
+
+    screen.exitonclick()
 
 
-# screen.exitonclick()
+if __name__ == "__main__":
+    start_app()

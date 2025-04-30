8e9ba1e711d15cd9b8d92bc4c2c6a846ad471619
serje <se25051990@gmail.com>
2025-04-30 16:11:36 +0300

    Day 30 - In progress

---
+++ intermediate/day_30_exceptions_json_data/main.py
@@ -0,0 +1,20 @@
+
+def start_app():
+    # FileNotFoundError
+    # with open("a_file.txt", "r") as file:
+    #     file.read()
+
+    # KeyError
+    # a_dict = {"key": "value"}
+    # value = a_dict["non_existing_key"]
+
+    # IndexError
+    # a_list = ["Alfa", "Bravo", "Charlie"]
+    # a_list[4]
+
+    # TypeError
+    print("some text" + 3)
+
+
+if __name__ == "__main__":
+    start_app()

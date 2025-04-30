4720051510802168c43f314695e702df55a1bfeb
serje <serje@mayflower.work>
2025-04-30 10:41:08 +0300

    Day 29 - in progress

--- intermediate/day_29_password_manager_tkinter_gui/password-manager-start/main.py
+++ intermediate/day_29_password_manager_tkinter_gui/password-manager-start/main.py
@@ -9,11 +9,11 @@ def start_app():
     # ---------------------------- UI SETUP ------------------------------- #
     window = Tk()
     window.title("Password Manager")
-    window.config(padx=20, pady=20)
+    window.config(padx=30, pady=30)
 
-    canvas = Canvas(width=300, height=300)
+    canvas = Canvas(width=200, height=200)
     logo_img = PhotoImage(file="logo.png")
-    canvas.create_image(200, 189, image=logo_img)
+    canvas.create_image(100, 100, image=logo_img)
     canvas.grid(row=0, column=1)  # To make an element using 2 columns - use 'columnspan'
 
     website_label = Label(text="Website:")
@@ -25,10 +25,10 @@ def start_app():
     password_label = Label(text="Password:")
     password_label.grid(row=3, column=0)
 
-    website_input = Entry(width=35)
+    website_input = Entry(width=40)
     website_input.grid(row=1, column=1, columnspan=2)
 
-    email_input = Entry(width=35)
+    email_input = Entry(width=40)
     email_input.grid(row=2, column=1, columnspan=2)
 
     password_input = Entry(width=21)
@@ -37,7 +37,7 @@ def start_app():
     gen_passw_button = Button(text="Generate Password", width=14)
     gen_passw_button.grid(row=3, column=2)
 
-    add_button = Button(text="Add", width=36)
+    add_button = Button(text="Add", width=38)
     add_button.grid(row=4, column=1, columnspan=2)
 
 

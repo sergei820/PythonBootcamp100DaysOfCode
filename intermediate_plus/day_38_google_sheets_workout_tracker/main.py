c268c1600b9ccec0ec8a80f110d79856b3cc9ad6
Sergei <se25051990@gmail.com>
2025-05-11 13:13:00 +0300

    Day 38 - Done

--- intermediate_plus/day_38_google_sheets_workout_tracker/main.py
+++ intermediate_plus/day_38_google_sheets_workout_tracker/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from os import environ
 from dotenv import load_dotenv
 import requests
@@ -9,6 +10,11 @@ API_KEY = environ.get("NUTRITIONIX_API_KEY")
 
 domain = "https://trackapi.nutritionix.com/"
 endpoint = "v2/natural/exercise"
+
+google_doc_id = environ.get("GOOGLE_DOC_ID")
+page_name = "page1"
+sheety_endpoint = f"https://api.sheety.co/{google_doc_id}/workoutTracking/{page_name}"
+sheety_bearer_auth_token = environ.get("SHEETY_BEARER_AUTH_TOKEN")
 
 def start_app():
     user_input = input("Tell me the exercise you did: ")
@@ -25,6 +31,38 @@ def start_app():
     response = requests.post(url=domain+endpoint, headers=headers, json=body)
     response.raise_for_status()
     print(response.text)
+
+    # Date	        Time	    Exercise	Duration	Calories
+    # 21/07/2020	15:00:00	Running	    22	        130
+
+    def write_to_google_docs(data: dict) -> None:
+        sheety_header = {
+            "Authorization": sheety_bearer_auth_token,
+        }
+        sheety_body = {
+            "page1": data,
+        }
+        sheety_response = requests.post(url=sheety_endpoint, json=sheety_body, headers=sheety_header)
+        print(sheety_response.status_code)
+
+    today = datetime.now()
+    date = today.strftime("%d/%m/%Y")
+    time = today.strftime("%H:%M:%S")
+
+    for exercise in response.json()["exercises"]:
+        exercise_name = exercise["name"]
+        duration = response.json()["exercises"][0]["duration_min"]
+        calories = response.json()["exercises"][0]["nf_calories"]
+
+        print(f"{date} {time} {exercise} {duration} {calories}")
+        exercise_data = {
+                "date": date,
+                "time": time,
+                "exercise": exercise_name.title(),
+                "duration": duration,
+                "calories": calories,
+            }
+        write_to_google_docs(exercise_data)
 
 
 if __name__ == "__main__":

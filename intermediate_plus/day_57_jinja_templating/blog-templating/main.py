d29a903c7cc2804fffb35400f4f76c2655ecb2b1
serje <se25051990@gmail.com>
2025-07-17 17:38:39 +0300

    Day 57 - In progress

--- intermediate_plus/day_57_jinja_templating/blog-templating/main.py
+++ intermediate_plus/day_57_jinja_templating/blog-templating/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
-
+from post import contents
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    all_posts = contents
+    return render_template("index.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)

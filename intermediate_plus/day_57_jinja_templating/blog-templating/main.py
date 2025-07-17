747926e7c6a7955f73b6b96aa5ce7f0a1be6e5f3
serje <se25051990@gmail.com>
2025-07-17 18:13:04 +0300

    Day 57 - Done

--- intermediate_plus/day_57_jinja_templating/blog-templating/main.py
+++ intermediate_plus/day_57_jinja_templating/blog-templating/main.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
-from post import contents
+from static.content import contents as all_posts
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    all_posts = contents
     return render_template("index.html", posts=all_posts)
+
+@app.route('/post/<post_id>')
+def get_post(post_id):
+    post = all_posts[int(post_id) - 1]
+    return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)

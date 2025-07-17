e5c6adab6f18792c35cd09115416b1e2e4e00f48
serje <se25051990@gmail.com>
2025-07-17 18:27:56 +0300

    Day 57 - Done

--- intermediate_plus/day_57_jinja_templating/blog-templating/post.py
+++ intermediate_plus/day_57_jinja_templating/blog-templating/post.py
@@ -1,2 +1,16 @@
 class Post:
-    pass
+    def __init__(self, post_id, title, subtitle, body):
+        self.id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
+
+def parse_json_posts(json_posts_list: list) -> list:
+    """Modifies a list of JSON posts to a list of class Post posts"""
+    posts_list = []
+    for post in json_posts_list:
+        posts_list.append(Post(post["id"], post["title"], post["subtitle"], post["body"]))
+
+    return posts_list
+
+

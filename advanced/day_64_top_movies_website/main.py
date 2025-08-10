69f6ebcb12d84823a276b4c768d2aef33a566803
serje <se25051990@gmail.com>
2025-08-10 11:51:34 +0300

    Day 64 - /edit route fixing

--- advanced/day_64_top_movies_website/main.py
+++ advanced/day_64_top_movies_website/main.py
@@ -48,32 +48,32 @@ class Movie(db.Model):
     review: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
 
-# new_movie = Movie(
-#         title="Phone Booth",
-#         year=2002,
-#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#         rating=7.3,
-#         ranking=10,
-#         review="My favourite character was the caller.",
-#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-#     )
+new_movie = Movie(
+        title="Phone Booth",
+        year=2002,
+        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+        rating=7.3,
+        ranking=1,
+        review="My favourite character was the caller.",
+        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    )
 
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
+second_movie = Movie(
+    title="Avatar The Way of Water",
+    year=2022,
+    description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+    rating=7.3,
+    ranking=2,
+    review="I liked the water.",
+    img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+)
 
-# with app.app_context():
-#     db.create_all()
-#
-# with app.app_context():
-#    db.session.add(second_movie)
-#    db.session.commit()
+with app.app_context():
+    db.create_all()
+
+with app.app_context():
+   db.session.add(second_movie)
+   db.session.commit()
 
 class RateMovieForm(FlaskForm):
     rating = StringField("Your rating up to 10")
